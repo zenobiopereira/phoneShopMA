e2ef55c5ab5533390b7d556077f30c7ee5b79421
zenobio <zenobio2190@gmail.com>
2018-10-13 20:35:23 -0300

    change method calling cart counter on navbar

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -2,17 +2,12 @@ import React, { Fragment } from 'react';
 import { Link } from "react-router-dom";
 
 const Navbar = ({id, name}) => {
-    var cout = 0;
-    id.map(obj => (obj) 
-    ? cout++
-    : cout
-    )
     return (
     <Fragment>
         <div className="mainDivNavbar">
         <h2> {name} </h2>
         <Link className="homeLink" to="/"> Home </Link>
-        <p>{cout}</p>
+        <p>{id.length}</p>
         <Link className="cartLink" to="/cart"> Cart </Link>
         </div>
     </Fragment>
